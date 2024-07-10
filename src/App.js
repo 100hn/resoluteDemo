b34f46a9dd31a7ac43076bb4bcf9ec18c8041bb5
100hn <srdhandhal2198@gmail.com>
2024-07-10 16:47:53 +0530

    him

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
       <Route  path='/career'Component={Career}/>
       <Route  path='/Pitch'Component={Pitch}/>
      </Routes>
+     <Home/>
      <Footer/>
      </BrowserRouter>
       
